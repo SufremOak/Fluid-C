137f0a03792fafed82e4186d7dfc2a63209aeeb9
SufremOak <migsufrem@gmail.com>
2025-02-22 09:27:55 -0300

    Create hello.cpp

---
+++ examples/hello.cpp
@@ -0,0 +1,12 @@
+// Fluid-C++ Hello World Example
+
+#include <FluidC++>
+
+@implementation sayHello
+   Log("Hello, Fluid-C++\n");
+@endImpl
+
+main <void> {
+  @invoque sayHello<void>;
+  exit 0;
+}
