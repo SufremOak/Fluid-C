26c86a04202d0b8864cbb3a10f8af0152a2bbc9e
SufremOak <migsufrem@gmail.com>
2025-04-02 14:24:38 -0300

    add MiniCompiler base

---
+++ Scripts/miniCompiler.sh
@@ -0,0 +1,15 @@
+#!/usr/local/env sh
+
+case "$1" in
+    --compile)
+        ;;
+    --jit)
+        ;;
+    --help)
+        ;;
+    --repl)
+        ;;
+    -V|--version)
+        echo "FluidScripten MiniCompiler v0.0.1-alpha.1"
+        exit 1
+        ;;
---
+++ src/compiler.h
@@ -0,0 +1,25 @@
+#ifndef COMPILER_H_
+#define COMPILER_H_
+
+namespace Compiler;
+
+public:
+    class compiler() {
+        static void parser() {
+            // ... parser
+        }
+
+        static void lexer() {
+            // ... lexer
+        }
+
+        static void ast() {
+            // ... ast
+        }
+
+        static void compilerMain() {
+            //
+        }
+    }
+
+#endif // COMPILER_H_
