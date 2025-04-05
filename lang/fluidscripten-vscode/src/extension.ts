8c1fc04a994f6db50e299378767d453c9da7d151
SufremOak <migsufrem@gmail.com>
2025-04-05 08:25:35 -0300

    Create extension.ts

---
+++ lang/fluidscripten-vscode/src/extension.ts
@@ -0,0 +1,18 @@
+import * as vscode from 'vscode';
+
+async function printDefinitionsForActiveEditor() {
+  const activeEditor = vscode.window.activeTextEditor;
+  if (!activeEditor) {
+    return;
+  }
+
+  const definitions = await vscode.commands.executeCommand<vscode.Location[]>(
+    'vscode.executeDefinitionProvider',
+    activeEditor.document.uri,
+    activeEditor.selection.active
+  );
+
+  for (const definition of definitions) {
+    console.log(definition);
+  }
+}
