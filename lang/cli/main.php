c4613bacfd7a1944e63e9ff2cc4d4551c9b031ec
SufremOak <migsufrem@gmail.com>
2025-04-05 08:28:31 -0300

    Create main.php

---
+++ lang/cli/main.php
@@ -0,0 +1,40 @@
+<?php 
+ namespace App\Commands;
+
+use Illuminate\Console\Scheduling\Schedule;
+use LaravelZero\Framework\Commands\Command;
+
+class InspiringCommand extends Command
+{
+    /**
+     * The signature of the command.
+     *
+     * @var string
+     */
+    protected $signature = 'inspiring {name=Artisan}';
+
+    /**
+     * The description of the command.
+     *
+     * @var string
+     */
+    protected $description = 'Display an inspiring quote';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        $this->info('Simplicity is the ultimate sophistication.');
+    }
+
+    /**
+     * Define the command's schedule.
+     */
+    public function schedule(Schedule $schedule): void
+    {
+        // $schedule->command(static::class)->everyMinute();
+    }
+}
+  
+/>
